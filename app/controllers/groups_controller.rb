365acd45a70f54c7fe9256bf4c34c9ac88bcd1d1
sudhir <sudhirsb2003@gmail.com>
2011-08-26 19:25:46 +0530

    made changes view  Please enter the commit message for your changes. Lines starting

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -44,7 +44,7 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to(@group, :notice => 'Group was successfully created.') }
+        format.html { redirect_to(@group, :notice => "#{@group.name} was successfully created.") }
         format.xml  { render :xml => @group, :status => :created, :location => @group }
       else
         format.html { render :action => "new" }
